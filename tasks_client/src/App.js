41685d63a2c565d957e66e6ec6b42092b0953e90
erikosampaio <erikoa.93@gmail.com>
2023-01-10 14:59:50 -0300

    Inicio a geração dos componentes do projeto.

--- tasks_client/src/App.js
+++ tasks_client/src/App.js
@@ -1,29 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+import Header from './components/header/Header';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
    
 library.add(faCheckCircle, faTrashAlt)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    );
+  }
 }
 
 export default App;
