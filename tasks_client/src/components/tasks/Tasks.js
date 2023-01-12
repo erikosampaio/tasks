a6aaa3e882ff3d67d816e9b7c4d35b0a3f0043ce
erikosampaio <erikoa.93@gmail.com>
2023-01-12 15:38:37 -0300

    Crio método para deletar todas as tasks dones de uma vez só.

--- tasks_client/src/components/tasks/Tasks.js
+++ tasks_client/src/components/tasks/Tasks.js
@@ -19,6 +19,31 @@ class Tasks extends Component {
     const tasks = await response.json();
     this.setState({ tasks: tasks });
   }
+
+  async deleteAllTaskDone() {
+    let response = await fetch(`http://localhost:3001/api/v1/tasks`);
+    const all_tasks = await response.json();
+    let done_tasks = [];
+    {
+      all_tasks.map((task, index) => {
+        if (task.done == true) {
+          done_tasks.push(task);
+        }
+      })
+    }
+    if (done_tasks.length === 0) {
+      window.alert(`Don't exists tasks to will be eraser!`);
+    } else {
+      if (window.confirm(`Are you sure you want to delete all tasks done?"`)) {
+        {
+          done_tasks.map((task, index) => {
+            fetch(`http://localhost:3001/api/v1/tasks/${task.id}`, {method: 'DELETE'});
+            this.loadTasks();
+          })
+        }
+      }
+    }
+  }
   
   componentDidMount() {
     this.loadTasks();
@@ -35,7 +60,7 @@ class Tasks extends Component {
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">Done</p>
           <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done == true)}/>
-          <Button variant="red" className="float-right remove_tasks_btn">Remove all tasks Done</Button>
+          <Button className="float-right remove_tasks_btn" onClick={() => this.deleteAllTaskDone()}>Remove all Tasks Done</Button>
         </Col>
       </Row>
     );
